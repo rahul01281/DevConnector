e788f2137be6d8f3caaf96b902add5945be3cf8c
Rahul Rinwa <rahul.rinwah@gmail.com>
2020-12-29 16:46:30 +0530

    add and delete profile education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -198,17 +198,57 @@ router.put('/experience', [ auth, [
     }
 });
 
-// @route     DELETE api/profile/experience/:exp_id
-// @desc      delete profile experience
+// @route     PUT api/profile/education
+// @desc      add profile experience
 // @access    private
-router.delete('/experience/:exp_id', auth, async (req, res) => {
+router.put('/education', [ auth, [
+    check('school', 'School is required').not().isEmpty(),
+    check('degree', 'Degree is requires').not().isEmpty(),
+    check('fieldofstudy', 'Field of study is requires').not().isEmpty(),
+    check('from', 'From Date is required').not().isEmpty()
+] ], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { school, degree, fieldofstudy, from, to, current, description } = req.body;
+
+    const newEdu = {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description
+    };
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        profile.education.unshift(newEdu);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('server error');
+    }
+});
+
+// @route     DELETE api/profile/education/:edu_id
+// @desc      delete profile education
+// @access    private
+router.delete('/education/:edu_id', auth, async (req, res) => {
     try {
         const profile = await Profile.findOne({ user: req.user.id });
 
         //get remove index
-        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id); //get the experience of that id
+        const removeIndex = profile.education.map(item => item.id).indexOf(req.params.exp_id); //get the experience of that id
 
-        profile.experience.splice(removeIndex, 1);
+        profile.education.splice(removeIndex, 1);
 
         await profile.save();
 
