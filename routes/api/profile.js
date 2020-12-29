f75bdf1b08e92b4121610fe87c49f07834bbacd0
Rahul Rinwa <rahul.rinwah@gmail.com>
2020-12-29 16:03:06 +0530

    add profile experiance

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -157,6 +157,45 @@ router.delete('/', auth, async (req, res) => {
         console.error(error.message);
         res.status(500).send('server error');
     }
-})
+});
+
+// @route     PUT api/profile/experience
+// @desc      add profile experience
+// @access    private
+router.put('/experience', [ auth, [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is requires').not().isEmpty(),
+    check('from', 'From Date is required').not().isEmpty()
+] ], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { title, company, location, from, to, current, description } = req.body;
+
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    };
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        profile.experience.unshift(newExp);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('server error');
+    }
+});
 
 module.exports = router;
