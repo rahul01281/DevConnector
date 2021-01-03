22f255dcde9f3eb64c0321fccab13b64f7ad1877
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-01-03 22:45:21 +0530

    get repos and get profiles actions and reducer

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,16 +2,54 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import {
     GET_PROFILE,
+    GET_PROFILES,
     PROFILE_ERROR,
     UPDATE_PROFILE,
     DELETE_ACCOUNT,
-    CLEAR_PROFILE
+    CLEAR_PROFILE,
+    GET_REPOS
 } from './types';
 
 //get the current user profile
 export const getCurrentProfile = () => async dispatch => {
     try {
         const res = await axios.get('/api/profile/me');
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status }
+        })
+    }
+}
+
+//get all profiles
+export const getProfiles = () => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
+
+    try {
+        const res = await axios.get('/api/profile');
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+        });
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status }
+        })
+    }
+}
+
+//get profile by id
+export const getProfilesById = (userId) => async dispatch => {    
+    try {
+        const res = await axios.get(`/api/profile/${userId}`);
 
         dispatch({
             type: GET_PROFILE,
@@ -190,4 +228,21 @@ export const deleteAccount = () => async dispatch => {
         }
     }
     
+};
+
+//get github repos
+export const getGithubRepos = (username) => async dispatch => {    
+    try {
+        const res = await axios.get(`api/profile/github/${username}`);
+
+        dispatch({
+            type: GET_REPOS,
+            payload: res.data
+        });
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status }
+        })
+    }
 }
