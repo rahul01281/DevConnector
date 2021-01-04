c1e2d915043f9c7289a7a8f2097642ec454b1c04
Rahul Rinwa <rahul.rinwah@gmail.com>
2021-01-04 22:21:34 +0530

    comment display and delete

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import { getPost } from '../../actions/post';
 import PostItem from '../posts/PostItem';
 import { Link } from 'react-router-dom';
 import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 
 function Post({ getPost, post: { post, loading }, match }) {
 
@@ -17,6 +18,11 @@ function Post({ getPost, post: { post, loading }, match }) {
         <Link to='/posts' className='btn'>Back to Posts</Link>
         <PostItem post={post} showActions={false} />
         <CommentForm postId={post._id} />
+        <div className="comments">
+            {post.comments.map(comment => (
+                <CommentItem key={comment._id} comment={comment} postId={post._id} />
+            ))}
+        </div>
     </Fragment>
 };
 
